380121f211682b6509d22175a9c9a2764ef1cfe4
neerajkorrapati <neerajkorrapati36@gmail.com>
2026-03-11 22:02:58 +0530

    Update cache_simulator.py
    adding an adaptive system for cache

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 
+
 def simulate_cache(sequence, cache_size):
 
     cache = OrderedDict()
@@ -9,10 +10,12 @@ def simulate_cache(sequence, cache_size):
     for block in sequence:
 
         if block in cache:
+
             hits += 1
             cache.move_to_end(block)
 
         else:
+
             misses += 1
 
             if len(cache) >= cache_size:
@@ -39,3 +42,23 @@ def analyze_cache_sizes(sequence, max_cache_size):
         results[size] = hit_rate
 
     return results
+
+
+def adaptive_cache(sequence, max_cache_size):
+
+    best_size = 1
+    best_hit = 0
+
+    history = []
+
+    for size in range(1, max_cache_size + 1):
+
+        hit_rate = simulate_cache(sequence, size)
+
+        history.append((size, hit_rate))
+
+        if hit_rate > best_hit:
+            best_hit = hit_rate
+            best_size = size
+
+    return best_size, best_hit, history
